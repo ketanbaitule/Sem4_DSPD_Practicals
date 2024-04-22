15ebebc0c2ab30a2899786ec640ebad1af590697
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2024-04-22 15:32:11 +0530

    Prac11: Added Practical 11

---
+++ Practical_11/prac11.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100  
+
+typedef struct Node {
+    int vertex;
+    struct Node* next;
+} Node;
+
+typedef struct Graph {
+    int numVertices;
+    Node** adjLists;
+    int* visited;
+} Graph;
+
+Node* createNode(int v) {
+    Node* newNode = malloc(sizeof(Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+Graph* createGraph(int vertices) {
+    Graph* graph = malloc(sizeof(Graph));
+    graph->numVertices = vertices;
+
+    graph->adjLists = malloc(vertices * sizeof(Node*));
+    graph->visited = malloc(vertices * sizeof(int));
+
+    int i;
+    for (i = 0; i < vertices; i++) {
+        graph->adjLists[i] = NULL;
+        graph->visited[i] = 0;
+    }
+
+    return graph;
+}
+
+void addEdge(Graph* graph, int src, int dest) {
+    Node* newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+
+    newNode = createNode(src);
+    newNode->next = graph->adjLists[dest];
+    graph->adjLists[dest] = newNode;
+}
+
+void DFS(Graph* graph, int vertex) {
+    Node* adjList = graph->adjLists[vertex];
+    Node* temp = adjList;
+
+    graph->visited[vertex] = 1;
+    printf(" %d\t", vertex);
+
+    while(temp != NULL) {
+        int connectedVertex = temp->vertex;
+
+        if(graph->visited[connectedVertex] == 0) {
+            DFS(graph, connectedVertex);
+        }
+        temp = temp->next;
+    }       
+}
+
+int main() {
+    Graph* graph = createGraph(6);
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 2);
+    addEdge(graph, 0, 3);
+    addEdge(graph, 1, 2);
+    addEdge(graph, 2, 4);
+
+    printf("DFS: ");
+    DFS(graph, 0);
+
+    return 0;
+}
